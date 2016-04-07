5b97a126aff0ed49d0cd21e84d1d34785300d497
ishani <ishanivyas@gmail.com>
2016-04-07 13:00:07 -0700

    fixed code for port

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ var express = require("express");
 
 var app=express();
 
+var quotes = [ "The best preparation for tomorrow is doing your best today.",
+    "I can resist everything except temptation.",
+    "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    "Either you run the day or the day runs you."];
+
+var show_quotes = quotes[Math.floor(Math.random() * quotes.length)];
+
+
 /** get corresponds to http method */
 app.get("/",function (req,res) {
+    res.render('index',{quote: show_quotes});
     res.sendFile(__dirname+"/index.html");
 });
 
